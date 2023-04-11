1375a633b18e9ede16c17af37d8e030b4c1503a1
bcaoa1 <bcaoa1@iscte-iul.pt>
2023-04-11 10:31:47 +0100

    Libraries e Modelo de Dados
    Librarie JSON - gson
    Librarie CSV - commons-csv

---
+++ GestaoHorarios/src/main/java/Horario.java
@@ -0,0 +1,47 @@
+import java.util.List;
+
+public class Horario {
+	
+	private List<Aula> horario;
+	
+	//ponto 6
+	public static Horario getHorarioFromJsonLocal(String path) {
+		return null;
+	}
+	
+	//ponto 7
+	public static Horario getHorarioFromJsonRemoto(String url) {
+		return null;
+	}
+	
+	//ponto 8
+	public static Horario getHorarioFromCsvLocal(String path) {
+		return null;
+	}
+	
+	//ponto 9
+	public static Horario getHorarioFromCsvRemoto(String url) {
+		return null;
+	}
+	
+	//ponto 10
+		public void saveToCsvLocal(String path) {
+			
+	}
+		
+	//ponto 11
+	public void saveToCsvRemoto(String url) {
+			
+	}
+	
+	//ponto 12
+	public void saveToJsonLocal(String path) {
+		
+	}
+	
+	//ponto 13
+	public void saveToJsonRemoto(String url) {
+		
+	}
+	
+}
