3082b0efa2f915e486760aeb2666079486b45f52
hzgge1 <hzgge1@iscte-iul.pt>
2023-04-11 22:48:04 +0100

    altrei

--- GestaoHorarios/src/main/java/ReadFileJsonRemote.java
+++ GestaoHorarios/src/main/java/ReadFileJsonRemote.java
@@ -1,14 +1,13 @@
-package ReadFileRemote;
-
 import java.util.*;
 import java.net.*;
 import java.io.*;
+import java.time.*;
 import com.google.gson.*;
 
 public class ReadFileJsonRemote {
 
 	private URL url;
-	private List<JsonObject> list = new ArrayList<>();
+	private List<Aula> list = new ArrayList<>();
 	private Gson gson = new Gson();
 	
 	public ReadFileJsonRemote(URL url) {
@@ -21,11 +20,28 @@ public class ReadFileJsonRemote {
 		
 		JsonObject json;
 		while((json = gson.fromJson(in.readLine(), JsonObject.class)) != null) {
-			list.add(json);
+			// funcao validacao do json
+			list.add(jsonToAula(json));
 		}
 	}
 	
-	public List<JsonObject> getListJsonObject() throws IOException {
+	private Aula jsonToAula(JsonObject json) {
+		String curso = json.get("Curso").toString();
+		String uc = json.get("Unidade Curricular").toString();
+		String turno = json.get("Turno").toString(); 
+		String turma = json.get("Turma").toString();
+		Integer inscritos = Integer.parseInt(json.get("Inscritos no turno").toString());
+		String diaSemana = json.get("Dia da semana").toString();
+		LocalTime horaInicio = LocalTime.parse(json.get("Hora início da aula").toString());
+		LocalTime horaFim = LocalTime.parse(json.get("Hora fim da aula").toString());
+		LocalDate dia = LocalDate.parse(json.get("Data da aula").toString());
+		String sala = json.get("Sala atribuída à aula").toString();
+		Integer lotacaoSala = Integer.parseInt(json.get("Lotação da sala").toString());
+		
+		return new Aula(curso,uc,turno,turma,inscritos,diaSemana,horaInicio,horaFim,dia,sala,lotacaoSala);
+	}
+	
+	public List<Aula> getListJsonObject() throws IOException {
 		readFromRemote();
 		return list;
 	}
