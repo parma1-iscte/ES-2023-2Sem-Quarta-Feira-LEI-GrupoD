e066842b83c1f5ddc89f725f07777f8f3b69d536
pamen <p.a.mendonc@gmail.com>
2023-04-14 17:24:38 +0100

    dei um trabalho nisto

--- GestaoHorarios/src/main/java/validarCSV.java
+++ GestaoHorarios/src/main/java/validarCSV.java
@@ -3,8 +3,6 @@ import java.io.File;
 import org.apache.commons.csv.*;
 import org.apache.commons.csv.CSVFormat;
 import java.nio.charset.Charset;
-
-
 /**
  * @author Pedro Mendonça
  * Desenvolver uma função que valida documentos de um ficheiro csv (se têm o formato correto)
@@ -44,7 +42,7 @@ public class validarCSV {
         }
 
 
-        if (!parser.getHeaderNames().containsAll(colunasDoCSV))
+        if (!parser.getHeaderNames().containsAll(colunasDoCSV))//TODO corrigir
             //https://docs.oracle.com/javase/8/docs/api/java/util/List.html#containsAll-java.util.Collection-
             /*
              * Se tiver colunas que não são as que era suposto ter
@@ -53,11 +51,38 @@ public class validarCSV {
              * o resto das veirficações.
              */
             return false;
+/*
+        List.of("Curso",
+                "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula",
+                "Data da aula","Lotação da sala", "Sala atribuída à aula");*/
+        List<CSVRecord> list = parser.getRecords();
+        for(CSVRecord record : list){
+           boolean isRecordValid =  !record.get("Curso").isEmpty() && !record.get("Unidade Curricular").isEmpty() &&
+                    !record.get("Turno").isEmpty()  &&  !record.get("Turma").isEmpty()  &&
+                    !record.get("Inscritos no turno").isEmpty() && ((int) record.get("Inscritos no turno")) >= 0 //TODO corrigir erro conhecido;
+                    &&  !record.get("Dia da semana").isEmpty() && !record.get("Hora início da aula").isEmpty() &&
+                    &&  !record.get("Hora fim da aula").isEmpty() && !record.get("Data da aula").isEmpty() &&
+                    &&  !record.get("Lotação da sala").isEmpty() && !record.get("Sala atribuída à aula").isEmpty() ;
+        }
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(validar());
+        CSVFormat format =   CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setDelimiter(';')
+                .build();
+        CSVParser parser = null;
+        try {
+            File csvData = new File("horario_exemplo.csv");
+            parser = CSVParser.parse(csvData, Charset.defaultCharset(),format);
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
+            //TODO
+        }
 
+       List<CSVRecord> list = parser.getRecords();
+       System.out.print( list.get(0));
     }
 }
