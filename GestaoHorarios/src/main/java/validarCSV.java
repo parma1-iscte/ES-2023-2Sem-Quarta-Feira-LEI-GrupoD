d074ed267fa9b64e6beacfbdfd468da85d525cfd
pamen <p.a.mendonc@gmail.com>
2023-04-14 03:11:09 +0100

    dei um trabalho nisto

--- GestaoHorarios/src/main/java/validarCSV.java
+++ GestaoHorarios/src/main/java/validarCSV.java
@@ -2,6 +2,9 @@ import java.util.*;
 import java.io.File;
 import org.apache.commons.csv.*;
 import org.apache.commons.csv.CSVFormat;
+import java.nio.charset.Charset;
+
+
 /**
  * @author Pedro Mendonça
  * Desenvolver uma função que valida documentos de um ficheiro csv (se têm o formato correto)
@@ -13,39 +16,47 @@ public class validarCSV {
     public static final validarCSV INSTANCE = new validarCSV();
     private static final List<String> colunasDoCSV = List.of("Curso",
             "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula",
-            "Data da aula", "Sala atribuída à aula", "Lotação da sala");
+            "Data da aula","Lotação da sala", "Sala atribuída à aula");
 
     private validarCSV() {
     }
 
     /*
     Validar:
-        Se tem as colunas todas
+        Se tem as colunas todas(falta ordenar as listas)
         Se campos corretos
 
      */
-    public static void validar() {
-
-
+    public static boolean validar() {
+        CSVFormat format =   CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setDelimiter(';')
+                .build();
         CSVParser parser = null;
         try {
-            parser = CSVParser.parse("./horario_exemplo.csv",CSVFormat.EXCEL);
+            File csvData = new File("horario_exemplo.csv");
+            parser = CSVParser.parse(csvData, Charset.defaultCharset(),format);
         } catch (java.lang.Exception e) {
             throw new RuntimeException(e);
+            //TODO
         }
-        System.out.println(parser.getHeaderNames());
 
 
+        if (!parser.getHeaderNames().containsAll(colunasDoCSV))
+            //https://docs.oracle.com/javase/8/docs/api/java/util/List.html#containsAll-java.util.Collection-
+            /*
+             * Se tiver colunas que não são as que era suposto ter
+             * (que são as guardadas na lista colunasDoCSV)
+             * retorna falso senão continua para
+             * o resto das veirficações.
+             */
+            return false;
+        return true;
     }
 
     public static void main(String[] args) {
+        System.out.println(validar());
 
-        CSVParser parser = null;
-        try {
-            parser = CSVParser.parse("./horario_exemplo.csv",CSVFormat.create().setHeader().setSkipHeaderRecord(true));
-        } catch (java.lang.Exception e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(parser.getHeaderNames().toString());
     }
 }
