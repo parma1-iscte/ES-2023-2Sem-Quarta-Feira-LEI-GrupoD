e3aa3639e88734ca0e4d13f1203e60dbc67c01f4
pamen <p.a.mendonc@gmail.com>
2023-04-14 19:01:00 +0100

    dei um trabalho nisto

--- GestaoHorarios/src/main/java/validarCSV.java
+++ GestaoHorarios/src/main/java/validarCSV.java
@@ -34,8 +34,8 @@ public class validarCSV {
                 .build();
         CSVParser parser = null;
         try {
-            File csvData = new File("horario_exemplo.csv");
-            parser = CSVParser.parse(csvData, Charset.defaultCharset(),format);
+            File csvData = new File("horario_exemplo1.csv");
+            parser = CSVParser.parse(csvData,Charset.defaultCharset() ,format);
         } catch (java.lang.Exception e) {
             throw new RuntimeException(e);
             //TODO
@@ -51,38 +51,31 @@ public class validarCSV {
              * o resto das veirficações.
              */
             return false;
-/*
-        List.of("Curso",
-                "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula",
-                "Data da aula","Lotação da sala", "Sala atribuída à aula");*/
+
         List<CSVRecord> list = parser.getRecords();
         for(CSVRecord record : list){
-           boolean isRecordValid =  !record.get("Curso").isEmpty() && !record.get("Unidade Curricular").isEmpty() &&
+            boolean isRecordValid = record.get("Curso") != null && record.get("Unidade Curricular") != null &&
+                    record.get("Turno") != null  && record.get("Turma") != null &&  record.get("Inscritos no turno") != null
+                    &&  record.get("Dia da semana") != null && record.get("Hora início da aula") != null
+                    &&  record.get("Hora fim da aula") != null && record.get("Data da aula") != null &&
+                    record.get("Lotação da sala") != null && record.get("Sala atribuída à aula") != null;
+
+
+            isRecordValid = isRecordValid &&
+                    !record.get("Curso").isEmpty() &&
+                    !record.get("Unidade Curricular").isEmpty() &&
                     !record.get("Turno").isEmpty()  &&  !record.get("Turma").isEmpty()  &&
-                    !record.get("Inscritos no turno").isEmpty() && ((int) record.get("Inscritos no turno")) >= 0 //TODO corrigir erro conhecido;
-                    &&  !record.get("Dia da semana").isEmpty() && !record.get("Hora início da aula").isEmpty() &&
+                    !record.get("Inscritos no turno").isEmpty()
+                    &&  !record.get("Dia da semana").isEmpty() && !record.get("Hora início da aula").isEmpty()
                     &&  !record.get("Hora fim da aula").isEmpty() && !record.get("Data da aula").isEmpty() &&
-                    &&  !record.get("Lotação da sala").isEmpty() && !record.get("Sala atribuída à aula").isEmpty() ;
+                    !record.get("Lotação da sala").isEmpty() && !record.get("Sala atribuída à aula").isEmpty() ;
+            //TODO extra hora de inicio < hora de fim ou hora de inicio +1h30 = hora de fim?
+            //TODO extra sala formato de string certo?
+            isRecordValid = isRecordValid  &&  Integer.parseInt(record.get("Inscritos no turno")) >= 0 && Integer.parseInt(record.get("Lotação da sala")) >=0;
+
+
+           if(!isRecordValid) return false;
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        CSVFormat format =   CSVFormat.Builder.create()
-                .setHeader()
-                .setSkipHeaderRecord(true)
-                .setDelimiter(';')
-                .build();
-        CSVParser parser = null;
-        try {
-            File csvData = new File("horario_exemplo.csv");
-            parser = CSVParser.parse(csvData, Charset.defaultCharset(),format);
-        } catch (java.lang.Exception e) {
-            throw new RuntimeException(e);
-            //TODO
-        }
-
-       List<CSVRecord> list = parser.getRecords();
-       System.out.print( list.get(0));
     }
 }
