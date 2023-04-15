aef4541dd706141e0d84c274461d4c782301b61d
pamen <p.a.mendonc@gmail.com>
2023-04-15 03:13:43 +0100

    Pus as duas validações juntas (do csv e JSON), passando o código que tinha feito para aqui e apaguando a minha classe. Também fiz esta classe singleton.

--- GestaoHorarios/src/main/java/Validacao.java
+++ GestaoHorarios/src/main/java/Validacao.java
@@ -3,13 +3,21 @@ import java.io.IOException;
 import com.google.gson.JsonObject;
 
 /**
- * @author Carlos Henriques
- * Desenvolver uma função que valida documentos de um ficheiro JSON (se têm o formato correto)
- * (3 horas)
+ * @author Carlos Henriques e Pedro Mendonça
+ * Desenvolver uma função que valida documentos de um ficheiro JSON ou CSV (se têm o formato correto)
+ * (3 horas cada)
  */
 
-public class Validacao {
+public class Validacao{
+    // É singleton
 
+    public static final Validacao INSTANCIA = new Validacao();
+
+    private Validacao(){}
+
+    private static final List<String> colunasDoCSV = List.of("Curso",
+            "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula",
+            "Data da aula","Lotação da sala", "Sala atribuída à aula");
     public static boolean validarDocumento(JsonObject objeto) {
         if (objeto.get("Curso").toString().isEmpty()) {
             //throw new RuntimeException("Curso é de preenchimento obrigatório");v
@@ -74,12 +82,67 @@ public class Validacao {
         }
 
         if (Integer.parseInt(objeto.get("Lotação da sala").toString()) < 0 && Integer.parseInt(objeto.get("Lotação da sala").toString()) > 250) {
-			//throw new RuntimeException("lotação inválida");
+            //throw new RuntimeException("lotação inválida");
             return false;
         }
         return true;
     }
 
+    public static boolean validarDocumento() {
+        //TODO por path ou File instance como argumento (uma versao para ambos prov)
+        CSVFormat format =   CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setDelimiter(';')
+                .build();
+        CSVParser parser = null;
+        try {
+            File csvData = new File("horario_exemplo1.csv");
+            parser = CSVParser.parse(csvData,Charset.defaultCharset() ,format);
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
+            //TODO
+        }
+
+
+        if (!parser.getHeaderNames().containsAll(colunasDoCSV))//TODO corrigir
+            //https://docs.oracle.com/javase/8/docs/api/java/util/List.html#containsAll-java.util.Collection-
+            /*
+             * Se tiver colunas que não são as que era suposto ter
+             * (que são as guardadas na lista colunasDoCSV)
+             * retorna falso senão continua para
+             * o resto das veirficações.
+             */
+            return false;
+
+        List<CSVRecord> list = parser.getRecords();
+        for(CSVRecord record : list){
+            boolean isRecordValid = record.get("Curso") != null && record.get("Unidade Curricular") != null &&
+                    record.get("Turno") != null  && record.get("Turma") != null &&  record.get("Inscritos no turno") != null
+                    &&  record.get("Dia da semana") != null && record.get("Hora início da aula") != null
+                    &&  record.get("Hora fim da aula") != null && record.get("Data da aula") != null &&
+                    record.get("Lotação da sala") != null && record.get("Sala atribuída à aula") != null;
+
+
+            isRecordValid = isRecordValid &&
+                    !record.get("Curso").isEmpty() &&
+                    !record.get("Unidade Curricular").isEmpty() &&
+                    !record.get("Turno").isEmpty()  &&  !record.get("Turma").isEmpty()  &&
+                    !record.get("Inscritos no turno").isEmpty()
+                    &&  !record.get("Dia da semana").isEmpty() && !record.get("Hora início da aula").isEmpty()
+                    &&  !record.get("Hora fim da aula").isEmpty() && !record.get("Data da aula").isEmpty() &&
+                    !record.get("Lotação da sala").isEmpty() && !record.get("Sala atribuída à aula").isEmpty() ;
+            //TODO extra hora de inicio < hora de fim ou hora de inicio +1h30 = hora de fim?
+            //TODO extra sala formato de string certo?
+            isRecordValid = isRecordValid  &&  Integer.parseInt(record.get("Inscritos no turno")) >= 0 && Integer.parseInt(record.get("Lotação da sala")) >=0;
+
+
+            if(!isRecordValid) return false;
+        }
+        return true;
+    }
+
+    //TODO Normalizar o documento Pedro e Henrique
 }
 
 
