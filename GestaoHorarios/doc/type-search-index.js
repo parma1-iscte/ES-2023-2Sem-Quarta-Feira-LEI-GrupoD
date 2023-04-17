edf600f29c7ef444caea41d93bc69b626a46d43d
N1ch0l4z <nseua@iscte-iul.pt>
2023-04-17 15:51:33 +0100

    Teste para ver se existe javadoc

---
+++ GestaoHorarios/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Horario","l":"Aula"},{"p":"Horario","l":"Horario"},{"p":"Horario","l":"Validacao"}];updateSearchResults();
