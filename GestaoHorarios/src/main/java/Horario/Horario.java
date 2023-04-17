bdd102a9a628425ce8f569e2b470c0a2be0cfd8d
andre <andre@DESKTOP-8RDJ79N.lan>
2023-04-17 16:37:42 +0100

    Dei fix a coisas pequenas so para ficar tudo igual

--- GestaoHorarios/src/main/java/Horario/Horario.java
+++ GestaoHorarios/src/main/java/Horario/Horario.java
@@ -97,9 +97,9 @@ public class Horario {
 	}
 
 	// ponto 8
-	public static Horario getHorarioFromCsvLocal(File file) throws FileNotFoundException, IOException {
+	public static Horario getHorarioFromCsvLocal(String pathCSV) throws FileNotFoundException, IOException {
 
-		BufferedReader in = new BufferedReader(new FileReader(file));
+		BufferedReader in = new BufferedReader(new FileReader(pathCSV));
 
 		return new Horario(readFileCsvWithBufferedReader(in));
 	}
@@ -160,9 +160,9 @@ public class Horario {
 	 * @param file o arquivo onde o horário será salvo.
 	 * @throws IOException se ocorrer um erro de I/O ao escrever no arquivo.
 	 */
-	public void saveToCsvLocal(File file) throws IOException {
+	public void saveToCsvLocal(String path) throws IOException {
 
-		PrintWriter writer = new PrintWriter(file);
+		PrintWriter writer = new PrintWriter(path);
 		CSVFormat csvFormat = CSVFormat.DEFAULT.withDelimiter(';').withHeader("Curso", "Unidade Curricular", "Turno", "Turma",
 				"Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula", "Data da aula", "Sala atríbuida à aula", "Lotacão da sala");
 		CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat);
@@ -222,9 +222,9 @@ public class Horario {
 	}
 
 	//ponto 12
-	public void saveToJsonLocal(File file, String jsonPath) throws IOException {
+	public void saveToJsonLocal(String pathCSV, String jsonPath) throws IOException {
 		
-		Horario horario = getHorarioFromCsvLocal(file);
+		Horario horario = getHorarioFromCsvLocal(pathCSV);
 		
 		Gson gson = new Gson();
 		String json = gson.toJson(horario);
@@ -237,10 +237,10 @@ public class Horario {
 	}
 
 	//ponto 13
-	public void saveToJsonRemoto(File file, String url) throws IOException, URISyntaxException{
+	public void saveToJsonRemoto(String pathCSV, String urlJson) throws IOException, URISyntaxException{
 		
-		Horario horario = getHorarioFromCsvLocal(file);
-		PrintWriter writer = new PrintWriter(new URI(url).toURL().openConnection().getOutputStream(),true, StandardCharsets.UTF_8);
+		Horario horario = getHorarioFromCsvLocal(pathCSV);
+		PrintWriter writer = new PrintWriter(new URI(urlJson).toURL().openConnection().getOutputStream(),true, StandardCharsets.UTF_8);
 		
 		Gson gson = new Gson();
 		String json = gson.toJson(horario);
