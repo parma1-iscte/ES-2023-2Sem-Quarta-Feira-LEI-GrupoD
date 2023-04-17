55807d9d8cca25401bf846b3bff7f3102e53f575
carlo <carlo@LAPTOP-R6CGGCG2>
2023-04-17 15:31:55 +0100

    classe Validação com mensagens em caso de falha

--- GestaoHorarios/src/main/java/Horario/Validacao.java
+++ GestaoHorarios/src/main/java/Horario/Validacao.java
@@ -27,30 +27,36 @@ public class Validacao{
     private static final List<String> colunasDoCSV = List.of("Curso",
             "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula",
             "Data da aula","Lotação da sala", "Sala atribuída à aula");
+    
     public static boolean validarDocumento(JsonObject objeto) {
         if (objeto.get("Curso").toString().isEmpty()) {
             //throw new RuntimeException("Curso é de preenchimento obrigatório");
+        	System.out.println("Curso é de preenchimento obrigatório");
             return false;
         }
 
         if (objeto.get("Unidade Curricular").toString().isEmpty()) {
             //throw new RuntimeException("A UC é de preenchimento obrigatório");
+        	System.out.println("A UC é de preenchimento obrigatório");
             return false;
         }
 
         if (objeto.get("Turma").toString().isEmpty()) {
             //throw new RuntimeException("Cada aula tem de ter uma turma associada e pode conter letras/números");
+        	System.out.println("Cada aula tem de ter uma turma associada e pode conter letras/números");
             return false;
         }
 
 
         if (objeto.get("Dia da semana").toString().isEmpty()) {
             //throw new RuntimeException("O dia da semana é de preenchimento obrigatório e tem de estar no formato (\"SEG,TER,QUA,QUI,SEX,SAB,DOM\") ");
-            return false;
+        	System.out.println("O dia da semana é de preenchimento obrigatório e tem de estar no formato (\"SEG,TER,QUA,QUI,SEX,SAB,DOM\") ");
+        	return false;
         }
 
         if (Integer.parseInt(objeto.get("Inscritos no turno").toString()) <= 0) { // podemos escolher o nº mínimo de alunos que é necessário para criar a aula???
             //throw new RuntimeException("Nº de inscritos inválido");
+        	System.out.println("Nº de inscritos inválido");
             return false;
         }
 
@@ -61,7 +67,8 @@ public class Validacao{
 
         if (objeto.get("Hora de início da aula").toString().isEmpty() || !(hora_inicio >= 0 && hora_inicio <= 23) || !(minutos_inicio >= 0 && minutos_inicio <= 59) || !(segundos_inicio >= 0 && segundos_inicio <= 59)) {
             //throw new RuntimeException("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
-            return false;
+        	System.out.println("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
+        	return false;
         }
 
         String[] hora_fim_aula = objeto.get("Hora de fim da aula").toString().split(":");
@@ -70,7 +77,8 @@ public class Validacao{
         int segundos_fim = Integer.parseInt(hora_inicio_aula[2]);
         if (objeto.get("Hora de fim da aula").toString().isEmpty() || !(hora_fim >= 0 && hora_fim <= 23) || !(minutos_fim >= 0 && minutos_fim <= 59) || !(segundos_fim >= 0 && segundos_fim <= 59)) {
             //throw new RuntimeException("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
-            return false;
+            System.out.println("A hora de fim da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
+        	return false;
         }
 
         String[] data_aula = objeto.get("Data da aula").toString().split("/");
@@ -79,7 +87,8 @@ public class Validacao{
         int ano_aula = Integer.parseInt(data_aula[2]);
         if (objeto.get("Data da aula").toString().isEmpty() || !(dia_aula >= 1 && dia_aula <= 31) || !(mes_aula >= 1 && mes_aula <= 12) || !(ano_aula >= 0 && ano_aula <= 2023)) {
             //throw new RuntimeException("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(dia/mes/ano)\", por exemplo, \"(12/12/2023)\" ");
-            return false;
+            System.out.println("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(dia/mes/ano)\", por exemplo, \"(12/12/2023)\" ");
+        	return false;
         }
 
         String[] sala_atribuida = objeto.get("Sala atribuída à aula").toString().split(".");
@@ -87,11 +96,13 @@ public class Validacao{
         int sala = Integer.parseInt(sala_atribuida[1]);
         if (!edificio_sala.startsWith("C") || !edificio_sala.startsWith("AA") || !edificio_sala.startsWith("D") || !edificio_sala.startsWith("Auditório") || !edificio_sala.startsWith("B") || !(sala >= 0 && sala <= 12)) {
             //throw new RuntimeException("Sala com formato incorreto, o formato tem de ser por exemplo \"C5.06\" ");
-            return false;
+            System.out.println("Sala com formato incorreto, o formato tem de ser por exemplo \"C5.06\" ");
+        	return false;
         }
 
         if (Integer.parseInt(objeto.get("Lotação da sala").toString()) < 0 && Integer.parseInt(objeto.get("Lotação da sala").toString()) > 250) {
             //throw new RuntimeException("lotação inválida");
+        	System.out.println("lotação inválida");
             return false;
         }
         return true;
