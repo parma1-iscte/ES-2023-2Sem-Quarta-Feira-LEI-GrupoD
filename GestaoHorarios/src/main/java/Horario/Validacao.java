b97afb0f0ad769c58e2997ab920bbc74a7157a6a
pamen <p.a.mendonc@gmail.com>
2023-04-18 11:16:14 +0100

    Avanço

--- GestaoHorarios/src/main/java/Horario/Validacao.java
+++ GestaoHorarios/src/main/java/Horario/Validacao.java
@@ -1,4 +1,5 @@
 package Horario;
+
 import java.io.*;
 import java.util.List;
 
@@ -13,50 +14,59 @@ import com.google.gson.JsonObject;
 /**
  * 
  * @author ES-2023-2Sem-Quarta-Feira-LEI-GrupoD
- * Versão 1.0
+ *         Versão 1.0
  */
 
-public class Validacao{
+public class Validacao {
 
     private static final List<String> colunasDoCSV = List.of("Curso",
-            "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula", "Hora fim da aula",
-            "Data da aula","Lotação da sala", "Sala atribuída à aula");
-    
+            "Unidade Curricular", "Turno", "Turma", "Inscritos no turno", "Dia da semana", "Hora início da aula",
+            "Hora fim da aula",
+            "Data da aula", "Lotação da sala", "Sala atribuída à aula");
+
     /**
-     * Esta funcao recebe como parametro um JsonObject e verifica se os seus campos estao validos
-     * @param objeto 
-     * @return boolean (se returnar true significa que o objeto tem os campos todos validos, ou seja, estao no formato correto, se for false esta invalido)
-     *  
+     * Esta funcao recebe como parametro um JsonObject e verifica se os seus campos
+     * estao validos
+     * 
+     * @param objeto
+     * @return boolean (se returnar true significa que o objeto tem os campos todos
+     *         validos, ou seja, estao no formato correto, se for false esta
+     *         invalido)
+     * 
      */
     public static boolean validarDocumento(JsonObject objeto) {
         if (objeto.get("Curso").toString().isEmpty()) {
-            //throw new RuntimeException("Curso é de preenchimento obrigatório");
-        	System.out.println("Curso é de preenchimento obrigatório");
+            // throw new RuntimeException("Curso é de preenchimento obrigatório");
+            System.out.println("Curso é de preenchimento obrigatório");
             return false;
         }
 
         if (objeto.get("Unidade Curricular").toString().isEmpty()) {
-            //throw new RuntimeException("A UC é de preenchimento obrigatório");
-        	System.out.println("A UC é de preenchimento obrigatório");
+            // throw new RuntimeException("A UC é de preenchimento obrigatório");
+            System.out.println("A UC é de preenchimento obrigatório");
             return false;
         }
 
         if (objeto.get("Turma").toString().isEmpty()) {
-            //throw new RuntimeException("Cada aula tem de ter uma turma associada e pode conter letras/números");
-        	System.out.println("Cada aula tem de ter uma turma associada e pode conter letras/números");
+            // throw new RuntimeException("Cada aula tem de ter uma turma associada e pode
+            // conter letras/números");
+            System.out.println("Cada aula tem de ter uma turma associada e pode conter letras/números");
             return false;
         }
 
-
         if (objeto.get("Dia da semana").toString().isEmpty()) {
-            //throw new RuntimeException("O dia da semana é de preenchimento obrigatório e tem de estar no formato (\"SEG,TER,QUA,QUI,SEX,SAB,DOM\") ");
-        	System.out.println("O dia da semana é de preenchimento obrigatório e tem de estar no formato (\"SEG,TER,QUA,QUI,SEX,SAB,DOM\") ");
-        	return false;
+            // throw new RuntimeException("O dia da semana é de preenchimento obrigatório e
+            // tem de estar no formato (\"SEG,TER,QUA,QUI,SEX,SAB,DOM\") ");
+            System.out.println(
+                    "O dia da semana é de preenchimento obrigatório e tem de estar no formato (\"SEG,TER,QUA,QUI,SEX,SAB,DOM\") ");
+            return false;
         }
 
-        if (Integer.parseInt(objeto.get("Inscritos no turno").toString()) <= 0) { // podemos escolher o nº mínimo de alunos que é necessário para criar a aula???
-            //throw new RuntimeException("Nº de inscritos inválido");
-        	System.out.println("Nº de inscritos inválido");
+        if (Integer.parseInt(objeto.get("Inscritos no turno").toString()) <= 0) { // podemos escolher o nº mínimo de
+                                                                                  // alunos que é necessário para criar
+                                                                                  // a aula???
+            // throw new RuntimeException("Nº de inscritos inválido");
+            System.out.println("Nº de inscritos inválido");
             return false;
         }
 
@@ -65,90 +75,112 @@ public class Validacao{
         int minutos_inicio = Integer.parseInt(hora_inicio_aula[1]);
         int segundos_inicio = Integer.parseInt(hora_inicio_aula[2]);
 
-        if (objeto.get("Hora de início da aula").toString().isEmpty() || !(hora_inicio >= 0 && hora_inicio <= 23) || !(minutos_inicio >= 0 && minutos_inicio <= 59) || !(segundos_inicio >= 0 && segundos_inicio <= 59)) {
-            //throw new RuntimeException("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
-        	System.out.println("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
-        	return false;
+        if (objeto.get("Hora de início da aula").toString().isEmpty() || !(hora_inicio >= 0 && hora_inicio <= 23)
+                || !(minutos_inicio >= 0 && minutos_inicio <= 59) || !(segundos_inicio >= 0 && segundos_inicio <= 59)) {
+            // throw new RuntimeException("A hora de início da aula é de preenchimento
+            // obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo,
+            // \"(12:59:06)\" ");
+            System.out.println(
+                    "A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
+            return false;
         }
 
         String[] hora_fim_aula = objeto.get("Hora de fim da aula").toString().split(":");
         int hora_fim = Integer.parseInt(hora_inicio_aula[0]);
         int minutos_fim = Integer.parseInt(hora_inicio_aula[1]);
         int segundos_fim = Integer.parseInt(hora_inicio_aula[2]);
-        if (objeto.get("Hora de fim da aula").toString().isEmpty() || !(hora_fim >= 0 && hora_fim <= 23) || !(minutos_fim >= 0 && minutos_fim <= 59) || !(segundos_fim >= 0 && segundos_fim <= 59)) {
-            //throw new RuntimeException("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
-            System.out.println("A hora de fim da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
-        	return false;
+        if (objeto.get("Hora de fim da aula").toString().isEmpty() || !(hora_fim >= 0 && hora_fim <= 23)
+                || !(minutos_fim >= 0 && minutos_fim <= 59) || !(segundos_fim >= 0 && segundos_fim <= 59)) {
+            // throw new RuntimeException("A hora de início da aula é de preenchimento
+            // obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo,
+            // \"(12:59:06)\" ");
+            System.out.println(
+                    "A hora de fim da aula é de preenchimento obrigatório e tem de estar no formato \"(hh:mm:ss)\", por exemplo, \"(12:59:06)\" ");
+            return false;
         }
 
         String[] data_aula = objeto.get("Data da aula").toString().split("/");
         int dia_aula = Integer.parseInt(data_aula[0]);
         int mes_aula = Integer.parseInt(data_aula[1]);
         int ano_aula = Integer.parseInt(data_aula[2]);
-        if (objeto.get("Data da aula").toString().isEmpty() || !(dia_aula >= 1 && dia_aula <= 31) || !(mes_aula >= 1 && mes_aula <= 12) || !(ano_aula >= 0 && ano_aula <= 2023)) {
-            //throw new RuntimeException("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(dia/mes/ano)\", por exemplo, \"(12/12/2023)\" ");
-            System.out.println("A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(dia/mes/ano)\", por exemplo, \"(12/12/2023)\" ");
-        	return false;
+        if (objeto.get("Data da aula").toString().isEmpty() || !(dia_aula >= 1 && dia_aula <= 31)
+                || !(mes_aula >= 1 && mes_aula <= 12) || !(ano_aula >= 0 && ano_aula <= 2023)) {
+            // throw new RuntimeException("A hora de início da aula é de preenchimento
+            // obrigatório e tem de estar no formato \"(dia/mes/ano)\", por exemplo,
+            // \"(12/12/2023)\" ");
+            System.out.println(
+                    "A hora de início da aula é de preenchimento obrigatório e tem de estar no formato \"(dia/mes/ano)\", por exemplo, \"(12/12/2023)\" ");
+            return false;
         }
 
         String[] sala_atribuida = objeto.get("Sala atribuída à aula").toString().split(".");
         String edificio_sala = sala_atribuida[0];
         int sala = Integer.parseInt(sala_atribuida[1]);
-        if (!edificio_sala.startsWith("C") || !edificio_sala.startsWith("AA") || !edificio_sala.startsWith("D") || !edificio_sala.startsWith("Auditório") || !edificio_sala.startsWith("B") || !(sala >= 0 && sala <= 12)) {
-            //throw new RuntimeException("Sala com formato incorreto, o formato tem de ser por exemplo \"C5.06\" ");
+        if (!edificio_sala.startsWith("C") || !edificio_sala.startsWith("AA") || !edificio_sala.startsWith("D")
+                || !edificio_sala.startsWith("Auditório") || !edificio_sala.startsWith("B")
+                || !(sala >= 0 && sala <= 12)) {
+            // throw new RuntimeException("Sala com formato incorreto, o formato tem de ser
+            // por exemplo \"C5.06\" ");
             System.out.println("Sala com formato incorreto, o formato tem de ser por exemplo \"C5.06\" ");
-        	return false;
+            return false;
         }
 
-        if (Integer.parseInt(objeto.get("Lotação da sala").toString()) < 0 && Integer.parseInt(objeto.get("Lotação da sala").toString()) > 250) {
-            //throw new RuntimeException("lotação inválida");
-        	System.out.println("lotação inválida");
+        if (Integer.parseInt(objeto.get("Lotação da sala").toString()) < 0
+                && Integer.parseInt(objeto.get("Lotação da sala").toString()) > 250) {
+            // throw new RuntimeException("lotação inválida");
+            System.out.println("lotação inválida");
             return false;
         }
         return true;
     }
 
+    public static boolean validarDocumento(CSVParser parser) {
+        /*
+         * CSVFormat format = CSVFormat.EXCEL
+         * .withHeader() //This causes the parser to read the first record and use its
+         * values as column names
+         * .withSkipHeaderRecord(true)
+         * .withDelimiter(';');
+         * CSVParser parser = null;
+         * try {
+         * parser = CSVParser.parse(csvData,Charset.defaultCharset(),format);
+         * } catch (IOException e) {
+         * throw new RuntimeException(e);
+         * }
+         */
 
-        public static boolean validarDocumento(CSVParser parser) {            
-                   /*  CSVFormat format =  CSVFormat.EXCEL
-                            .withHeader()  //This causes the parser to read the first record and use its values as column names
-                            .withSkipHeaderRecord(true)
-                            .withDelimiter(';');
-                    CSVParser parser = null;
-                    try {
-                        parser = CSVParser.parse(csvData,Charset.defaultCharset(),format);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-            */
-            
-                    if (!(parser.getHeaderNames().containsAll(colunasDoCSV) && colunasDoCSV.containsAll(parser.getHeaderNames())))
-                    /**
-                     * O interessante deste if é que garante antes de continuar a execução que o ficheiro csv que importamos
-                     * têm exatamente as colunas que é suposto ter (que são as guardadas na lista colunasDoCSV);
-                     * sem colunas a mais, a menos ou diferentes independente da ordem das colunas no ficheiro.
-                     * Retorna falso e termina a execução da função caso isso não seja garantido.
-                     */
-                    return false;
-            
-                    List<CSVRecord> list = parser.getRecords();
-                    for(CSVRecord record : list){
-                        if(!areAllFieldsNonNull(record)) return false;
-            
-                        if(!areAllFieldsNotEmptyStrings(record)) return false;
-            
-                        //TODO extra hora de inicio < hora de fim ou hora de inicio +1h30 = hora de fim?
-                        //TODO extra sala formato de string certo?
-                      boolean  areTheIntsValid =
-                                Integer.parseInt(record.get("Inscritos no turno")) >= 0
-                                && Integer.parseInt(record.get("Lotação da sala")) >=0;
-            
-            
-                        if(!areTheIntsValid) return false;
-                    }
-                    return true;
-                }
-   
+        if (!(parser.getHeaderNames().containsAll(colunasDoCSV) && colunasDoCSV.containsAll(parser.getHeaderNames())))
+            /**
+             * O interessante deste if é que garante antes de continuar a execução que o
+             * ficheiro csv que importamos
+             * têm exatamente as colunas que é suposto ter (que são as guardadas na lista
+             * colunasDoCSV);
+             * sem colunas a mais, a menos ou diferentes independente da ordem das colunas
+             * no ficheiro.
+             * Retorna falso e termina a execução da função caso isso não seja garantido.
+             */
+            return false;
+
+        List<CSVRecord> list = parser.getRecords();
+        for (CSVRecord record : list) {
+            if (!areAllFieldsNonNull(record))
+                return false;
+
+            if (!areAllFieldsNotEmptyStrings(record))
+                return false;
+
+            // TODO extra hora de inicio < hora de fim ou hora de inicio +1h30 = hora de
+            // fim?
+            // TODO extra sala formato de string certo?
+            boolean areTheIntsValid = Integer.parseInt(record.get("Inscritos no turno")) >= 0
+                    && Integer.parseInt(record.get("Lotação da sala")) >= 0;
+
+            if (!areTheIntsValid)
+                return false;
+        }
+        return true;
+    }
+
     private static boolean areAllFieldsNotEmptyStrings(CSVRecord record) {
         return !record.get("Curso").isEmpty() &&
                 !record.get("Unidade Curricular").isEmpty() &&
@@ -158,6 +190,7 @@ public class Validacao{
                 && !record.get("Hora fim da aula").isEmpty() && !record.get("Data da aula").isEmpty() &&
                 !record.get("Lotação da sala").isEmpty() && !record.get("Sala atribuída à aula").isEmpty();
     }
+
     private static boolean areAllFieldsNonNull(CSVRecord record) {
         return record.get("Curso") != null && record.get("Unidade Curricular") != null &&
                 record.get("Turno") != null && record.get("Turma") != null && record.get("Inscritos no turno") != null
@@ -166,25 +199,19 @@ public class Validacao{
                 record.get("Lotação da sala") != null && record.get("Sala atribuída à aula") != null;
     }
 
-   public static void main(String[] args) {
-    CSVFormat format =  CSVFormat.EXCEL
-    .withHeader()  //This causes the parser to read the first record and use its values as column names
-    .withSkipHeaderRecord(true)
-    .withDelimiter(';');
-CSVParser parser = null;
-try {
-File csvData = new File("linesSets.csv");
-parser = CSVParser.parse(csvData,Charset.defaultCharset(),format);
-} catch (IOException e) {
-throw new RuntimeException(e);
-}
-List<CSVRecord> lines = parser.getRecords();
-System.out.println(Validacao.validarCsvLine(lines.get(0))); //Record valido
-System.out.println(Validacao.validarCsvLine(lines.get(1))); //Record com um campo a null
-System.out.println(Validacao.validarCsvLine(lines.get(2))); // Record com um campo com empty String
-System.out.println(Validacao.validarCsvLine(lines.get(3))); // Record com campo numerico negativo
-}
+    public static void main(String[] args) {
+        CSVFormat format = CSVFormat.EXCEL
+                .withHeader() // This causes the parser to read the first record and use its values as column
+                              // names
+                .withSkipHeaderRecord(true)
+                .withDelimiter(';');
+        CSVParser parser = null;
+        try {
+            File csvData = new File("linesSets.csv");
+            parser = CSVParser.parse(csvData, Charset.defaultCharset(), format);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
-
-
-
+}
--- GestaoHorarios/src/test/java/Tests/ValidacaoTest.java
+++ GestaoHorarios/src/test/java/Tests/ValidacaoTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.*;
-import java.util.List;
 
 import java.nio.charset.Charset;
 
@@ -19,50 +18,113 @@ import Horario.*;
 /**
  * 
  * @author ES-2023-2Sem-Quarta-Feira-LEI-GrupoD
- * Versão 1.0
+ *         Versão 1.0
  */
 
-public  class ValidacaoTest {
-  
+public class ValidacaoTest {
+
+   private static  final CSVFormat format = CSVFormat.EXCEL
+    .withHeader() // This causes the parser to read the first record and use its values as column
+                  // names
+    .withSkipHeaderRecord(true)
+    .withDelimiter(';');
+    
     @Test
-    public  void testValidarCsvLine(){
-        CSVFormat format =  CSVFormat.EXCEL
-                .withHeader()  //This causes the parser to read the first record and use its values as column names
-                .withSkipHeaderRecord(true)
-                .withDelimiter(';');
-        CSVParser parser = null;
+    public void testValidarCSVCorreto() {
+        // Ficheiro valido
+        CSVParser correto = null;
         try {
-            File csvData = new File("linesSets.csv");
-            parser = CSVParser.parse(csvData,Charset.defaultCharset(),format);
+            File csvData = new File("Conjunto de teste/Correto.csv");
+            correto = CSVParser.parse(csvData, Charset.defaultCharset(), format);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        List<CSVRecord> lines = parser.getRecords();
-        assertTrue(Validacao.validarCsvLine(lines.get(0)));  //Record valido
-        assertFalse(Validacao.validarCsvLine(lines.get(1))); //Record com um campo a null
-        assertFalse(Validacao.validarCsvLine(lines.get(2))); // Record com um campo com empty String
-        assertFalse(Validacao.validarCsvLine(lines.get(3))); // Record com campo numerico negativo
+        assertTrue(Validacao.validarDocumento(correto));         
+    }
+    
+    @Test
+    private void validarCSVComHeaderAlterado() {
+        CSVParser headerAlterado = null;
+           try {
+               File csvData5 = new File("Conjunto de teste/Header com colunas a alteradas.csv");
+               headerAlterado = CSVParser.parse(csvData5, Charset.defaultCharset(), format);
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+           assertFalse(Validacao.validarDocumento(headerAlterado));
+    }
+    @Test
+    private void validarCSVComHeaderComLinhasAMais() {
+        CSVParser headerComRowsAmais = null;
+           try {
+               File csvData6 = new File("Conjunto de teste/Header com colunas a mais.csv");
+               headerComRowsAmais = CSVParser.parse(csvData6, Charset.defaultCharset(), format);
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+           assertFalse(Validacao.validarDocumento(headerComRowsAmais));
     }
 
     @Test
-    public   void testValidarCsvHeader(){
-
-        CSVFormat format =  CSVFormat.EXCEL
-        .withHeader()  //This causes the parser to read the first record and use its values as column names
-        .withSkipHeaderRecord(true)
-        .withDelimiter(';');
-CSVParser parser = null;
-try {
-    File csvData = new File("HeaderSet.csv");
-    parser = CSVParser.parse(csvData,Charset.defaultCharset(),format);
-} catch (IOException e) {
-    throw new RuntimeException(e);
-}
-List<CSVRecord> headers = parser.getRecords();
-        assertTrue(Validacao.validarCsvLine(headers.get(0)));  //Header correto
-        assertTrue(Validacao.validarCsvLine(headers.get(1)));  //Header com linhas desodenadas
-        assertFalse(Validacao.validarCsvLine(headers.get(2))); // Header com colunas a mais
-        assertFalse(Validacao.validarCsvLine(headers.get(3))); // Header com colunas a menos
-        assertFalse(Validacao.validarCsvLine(headers.get(4))); // Header com colunas a alteradas
+    private void validarCSVComHeaderComLinhasAMenos() {
+        CSVParser headerComRowsAmenos = null;
+           try {
+               File csvData7 = new File("Conjunto de teste/Header com colunas a menos.csv");
+               headerComRowsAmenos = CSVParser.parse(csvData7, Charset.defaultCharset(), format);
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+           assertFalse(Validacao.validarDocumento(headerComRowsAmenos));
     }
+    @Test
+    private void validarCSVComHeaderDesordenado() {
+        CSVParser headerDesordenado = null;
+           try {
+               File csvData8 = new File("Conjunto de teste/Header com linhas desordenadas.csv");
+               headerDesordenado = CSVParser.parse(csvData8, Charset.defaultCharset(), format);
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+           assertFalse(Validacao.validarDocumento(headerDesordenado));
+    }
+
+    @Test
+    private void validarCSVComFieldNull() {
+        // Record com um campo a null
+
+       CSVParser recordComNull = null;
+       try {
+           File csvData2 = new File("Conjunto de teste/Record com um campo a null.csv");
+           recordComNull = CSVParser.parse(csvData2, Charset.defaultCharset(), format);
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+       assertFalse(Validacao.validarDocumento(recordComNull));
+    }
+
+    @Test
+    private void validarCSVComEmptyString() {
+        // Record com um campo com empty String
+         CSVParser recordComEmptyString = null;
+        try {
+            File csvData3 = new File("Conjunto de teste/Record com um campo com empty String.csv");
+            recordComEmptyString = CSVParser.parse(csvData3, Charset.defaultCharset(), format);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertFalse(Validacao.validarDocumento(recordComEmptyString));
+    }
+    @Test
+    private void validarCSVComFieldNegativo() {
+        // Record com campo numerico negativo
+           CSVParser recordComNumNeg = null;
+           try {
+               File csvData4 = new File("Conjunto de teste/Record com campo numerico negativo.csv");
+               recordComNumNeg = CSVParser.parse(csvData4, Charset.defaultCharset(), format);
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+           assertFalse(Validacao.validarDocumento(recordComNumNeg));
+    }
+
 }
